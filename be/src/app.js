d56854aeebbb3617c7d3907321b8de2534dd5113
Vinh Nguyen Duc <nguyenducvinh77102006@gmail.com>
2026-05-29 10:26:20 +0700

    update infor db

--- be/src/app.js
+++ be/src/app.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import passport from 'passport';
+import cors from 'cors';
 import './config/passport.js';
 import router from './routes/index.js';
 
 const app = express();
+
+app.use(cors({
+  origin: process.env.BASE_URL_FE, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(passport.initialize());
